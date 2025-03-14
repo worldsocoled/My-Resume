9b53f0b4c70b1c5f05d097c3960f6a789141f8b7
Nichole (Cole) Higgins <154705316+worldsocoled@users.noreply.github.com>
2025-03-13 21:04:26 -0400

    Updated script.js

--- Code Files/script.js
+++ Code Files/script.js
@@ -3,3 +3,17 @@ document.querySelectorAll('.technical-skills li, .soft-skills li').forEach(item 
         item.classList.toggle('highlight');
     });
 });
+
+window.onscroll = function() {
+    const button = document.getElementById('backToTop');
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        button.style.display = "block";
+    } else {
+        button.style.display = "none";
+    }
+};
+
+document.getElementById('backToTop').onclick = function() {
+    document.body.scrollTop = 0; 
+    document.documentElement.scrollTop = 0; 
+};
